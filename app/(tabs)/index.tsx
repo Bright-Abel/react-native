b30bdd1be086f8213ab74b06ef753a27c47504dc
bright-abel <oluwasegunbarnabas@gmail.com>
2026-04-20 12:44:46 +0100

    feat: implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text } from 'react-native';
 export default function App() {
   return (
     <SafeAreaViewLayout>
-      <Text className='text-xl font-bold text-blue-500'>
+      <Text className='text-xl font-sans-extrabold text-blue-500'>
         Welcome to Nativewind!
       </Text>
     </SafeAreaViewLayout>
